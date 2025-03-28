f48cee1e931a243122f84e54b60d0bb7d0f315c1
João Pedro Cazarotti Batisata <joaopedrocazarottijp@gmail.com>
2025-03-28 16:24:57 -0300

    return do main excluído

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:appbook/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Lista de Livros",
